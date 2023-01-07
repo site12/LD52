9a480b156489ad7707f62f93c4786b5a06a6031c
jkazuk <jeffreykazuk@gmail.com>
2023-01-06 23:56:19 -0600

    fixed sounds

--- objects/weapons/sounds/hoe/swing.gd
+++ objects/weapons/sounds/hoe/swing.gd
@@ -1,10 +1,9 @@
 extends AudioStreamPlayer3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	autoplay = false
-	await get_tree().create_timer(0.56).timeout
+	await get_tree().create_timer(0.28).timeout
 	self.queue_free()
 
 
