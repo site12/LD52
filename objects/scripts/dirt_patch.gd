82c7297f97cdf31f54e844902f4a8fb38d029f65
jkazuk <jeffreykazuk@gmail.com>
2023-01-08 17:05:15 -0600

    fixed the thing

--- objects/scripts/dirt_patch.gd
+++ objects/scripts/dirt_patch.gd
@@ -35,7 +35,7 @@ var dirt_colors = {
 }
 
 var dirt_state = "raw"
-var highlight:StandardMaterial3D = preload("res://objects/interactables/materials/new_standard_material_3d.tres")
+var highlight:StandardMaterial3D = preload("res://objects/interactables/materials/highlight.tres")
 
 func get_class(): return "DirtPatch"
 
