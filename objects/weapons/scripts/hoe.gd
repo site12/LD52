4fb5512c8c4b803ce241c96231bce9edb0daffc6
Shane <shanescott86@gmail.com>
2023-01-08 02:22:40 -0600

    added scythe

--- objects/weapons/scripts/hoe.gd
+++ objects/weapons/scripts/hoe.gd
@@ -2,6 +2,10 @@
 
 extends Gun
 
+var mat_rust = preload("res://objects/weapons/mats/hoe_tool_rust.tres")
+var mat_iron = preload("res://objects/weapons/mats/hoe_tool_iron.tres")
+var mat_silver = preload("res://objects/weapons/mats/hoe_tool_silver.tres")
+
 #this handles the animations every frame and handles the player state and updates the ammo count
 func _physics_process(delta):
 	walk()
