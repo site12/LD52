d8bc7686db90883782285422aee64b3a0790bfce
jkazuk <jeffreykazuk@gmail.com>
2023-01-08 02:22:24 -0600

    carrot enemy following and attacking

--- entities/scripts/carrot_enemy.gd
+++ entities/scripts/carrot_enemy.gd
@@ -8,3 +8,9 @@ class_name CarrotEnemy
 func _ready() -> void:
 	enemy_val = 750
 
+func anim_process():
+	if velocity.length() > 0 and $timers/attack_timer.is_stopped():
+		$carrot_man/AnimationTree1["parameters/playback"].travel("carrotrun1")
+
+func run_attack_anim():
+	$carrot_man/AnimationTree1["parameters/playback"].travel("carrotattack")
--- entities/scripts/corn_enemy.gd
+++ entities/scripts/corn_enemy.gd
@@ -1,7 +1,16 @@
 extends Enemy
 class_name CornEnemy
 
-@onready var carrot_scene = $carrot_model
 
-func _physics_process(delta):
-	pass
+
+# @onready var carrot_scene = $carrot_man
+
+func _ready() -> void:
+	enemy_val = 750
+
+func anim_process():
+	if velocity.length() > 0 and $timers/attack_timer.is_stopped():
+		$corn_model/AnimationTree["parameters/playback"].travel("cornrun2")
+
+func run_attack_anim():
+	$corn_model/AnimationTree["parameters/playback"].travel("cornattack")
